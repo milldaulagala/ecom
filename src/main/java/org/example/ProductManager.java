029e8dbb24ec116ce97b5343df4dd84ee9ff15c5
Mill Daulagala <mill.daulagala@gmail.com>
2024-02-15 21:06:54 +1100

    created the project structure

---
+++ src/main/java/org/example/ProductManager.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ProductManager {
+    public boolean addProduct(String item, String type, int price, int discount) {
+        return true;
+    }
+
+    public int getNumberOfProducts() {
+        return 2;
+    }
+}
