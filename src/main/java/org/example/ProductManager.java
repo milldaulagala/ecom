2e151968dcaaeafdb5cc97cc47dc01564027437a
Mill Daulagala <mill.daulagala@gmail.com>
2024-03-01 16:01:30 +1100

    created itemName array

--- src/main/java/org/example/ProductManager.java
+++ src/main/java/org/example/ProductManager.java
@@ -1,11 +1,23 @@
 package org.example;
 
 public class ProductManager {
+
+    private int count = 0;
+    private final String[] itemName = new String[100];
+
     public boolean addProduct(String item, String type, int price, int discount) {
+
+        itemName[count] = item;
+        count = count + 1 ;
         return true;
     }
 
     public int getNumberOfProducts() {
-        return 2;
+        return count;
     }
+
+    public String getProduct(int i) {
+        return itemName[i];
+    }
+
 }
---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.example.ProductManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProductManagerTest {
+    @Test
+    public void testAddProductCountSuccess(){
+        //given
+        ProductManager pm = new ProductManager();
+        //when
+        boolean result = pm.addProduct("Fan", "5 speed Fan", 15, 0);
+        boolean result1 = pm.addProduct("Fan", "5 speed Fan", 15, 0);
+        boolean result2 = pm.addProduct("Fan", "5 speed Fan", 15, 0);
+
+        //then
+        Assertions.assertTrue(result);
+        Assertions.assertTrue(result1);
+        Assertions.assertTrue(result2);
+
+        Assertions.assertTrue(pm.getNumberOfProducts()>0);
+        assertEquals(3, pm.getNumberOfProducts());
+    }
+
+    @Test
+    public void testAddProductGetSuccess() {
+
+        //Given
+        ProductManager pm = new ProductManager();
+        boolean result = pm.addProduct("laptop","computer",1000,10);
+        boolean result1 = pm.addProduct("monitor","computer",1000,10);
+
+        //When
+        String name1 = pm.getProduct(0);
+        String name2 = pm.getProduct(1);
+
+        //Then
+        assertTrue(result);
+        assertEquals("laptop",name1);
+        assertEquals("monitor",name2);
+    }
+}
